a0bb3d742aa77d2fcd2bbfc84b9e8383c57d2393
Marcelo Gomes <philipsmonster@gmail.com>
2020-11-12 07:40:11 -0300

    projeto interface

---
+++ src/main/java/entities/Contract.java
@@ -0,0 +1,51 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Contract {
+
+    private Integer number;
+    private Date data;
+    private Double totalValue;
+
+    List<Installment> installments = new ArrayList<Installment>();
+
+    public Contract() {
+    }
+
+    public Contract(Integer number, Date data, Double totalValue) {
+        this.number = number;
+        this.data = data;
+        this.totalValue = totalValue;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(Double totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public List<Installment> getInstallments() {
+        return installments;
+    }
+}
